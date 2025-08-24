f6f74f35f4bbb76a03db13f048baf5cf4d62061e
Arif Ansari <arifansari10027@gmail.com>
2025-08-24 15:02:32 +0530

    Update of bot code for dB.

--- app/bot.py
+++ app/bot.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from app.handlers.textHandler import register_text_handler
 from app.handlers.voiceHandler import register_voice_handler
 from app.handlers.imageHandler import register_image_handler
-from app.services.database import save_message
+from app.services.database import MessageLog, save_message
 
 load_dotenv()
 
@@ -18,11 +18,6 @@ register_text_handler(bot)
 register_voice_handler(bot)
 register_image_handler(bot)
 
-if __name__ == "__main__":
-    init_db()
-    print("Database initialized.")
-    bot.infinity_polling()
-
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, "Hello! I am Nalayak but I am very layak :)")
@@ -31,5 +26,19 @@ def start(message):
 def image(message):
     bot.reply_to(message, "Please send me an image.")
 
-print("Bot is running...")
-bot.polling()
+def save_message(user_id: int, message_type: str, content: str):
+    try:
+        db = SessionLocal()
+        log = MessageLog(user_id=str(user_id), message_type=message_type, content=content)
+        db.add(log)
+        db.commit()
+        db.refresh(log)
+        db.close()
+
+        print(f"[DB] Saved message -> {message_type} from {user_id}")
+    except Exception as e:
+        print(f"[DB ERROR] {e}")
+
+if __name__ == "__main__":
+    print("Bot is running...")
+    bot.polling()
